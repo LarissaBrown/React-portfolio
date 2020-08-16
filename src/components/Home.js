63a192403acee0446051b6e2b98a077b81e26407
Larissa Brown <larisa.brown.johnson@gmail.com>
2020-08-16 11:38:48 -0600

    new text Experience

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function MainContent() {
         <div  className="mainContent">
             <div id="myName" className="main">My name is Larissa Brown <br/>
                 <span style={{color: "WhiteSmoke"}}>I am a Software Developer</span><br/>
-                <span style={{color: "WhiteSmoke"}}>Let me show you what I do.</span>
+                <span style={{color: "WhiteSmoke"}}>Experience what I do.</span>
             </div>
             <div id="div1"
                 className="main">
